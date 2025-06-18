1ccedb77437a57e678cd255550d32e05568903b6
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-06-17 20:22:28 -1200

    commit

--- api-gateway/src/main/java/com/amol/apis_gateway/config/RestTemplateConfig.java
+++ api-gateway/src/main/java/com/amol/apis_gateway/config/RestTemplateConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class RestTemplateConfig {
     @Bean
-//    @LoadBalanced
+
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
